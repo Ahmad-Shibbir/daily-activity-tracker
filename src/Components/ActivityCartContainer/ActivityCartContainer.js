b5b175ef61b41c4ae2bb09ef4818882feab554b8
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-09-28 23:46:07 +0800

    requered component files created

---
+++ src/Components/ActivityCartContainer/ActivityCartContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ActivityCartContainer.css'
+const ActivityCartContainer = () => {
+    return (
+        <div>
+            <h1>inside activity cart</h1>
+        </div>
+    );
+};
+
+export default ActivityCartContainer;
---
+++ src/Components/ActivityContainer/ActivityContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ActivityContainer.css';
+
+const ActivityContainer = () => {
+    return (
+        <div>
+            <h1>inside activityContainer</h1>
+        </div>
+    );
+};
+
+export default ActivityContainer;
